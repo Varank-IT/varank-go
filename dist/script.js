018024a60bfd3b3635ef43a6d99af4a945e14868
Alphen <85937475+AlphenLLC@users.noreply.github.com>
2022-04-14 22:13:32 +0300

    TEAM VARANK: THE SUFFERING

--- dist/script.js
+++ dist/script.js
@@ -6,6 +6,14 @@ Array.from(collapsibleHeaders).forEach(header => {
     });
 });
 
+$('.tarkikComandSlider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+});
+
 /*----------------------------------------------------*/
 /* Quote Loop
 ------------------------------------------------------ */
